dbb52cff70132aca1e0e546dba0df43520496d23
Lamine ADEL <lamine.adel@ynov.com>
2022-02-07 23:26:07 +0100

    login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from "./index.module.scss";
 import HeaderLogo from "../../components/header/HeaderLogo/HeaderLogo";
+import Input from '../../components/UI/Input';
+import fb from '../../../public/FB.png'
+
 const index = () => {
     return (
         <div className={styles.body}>
@@ -8,10 +11,28 @@ const index = () => {
                 <HeaderLogo />
             </div>
             <div className={styles.signinform}>
+                <h1 className={styles.formh1}>S'identifier</h1>
                 <form className={styles.form}>
-                    <h1 className={styles.formh1}>S'identifier</h1>
-
+                    <Input type="email" placeholder="E-mail ou numero telephone" />
+                    <Input type="password" placeholder='Mot de passe' />
+                    <button className="btn btn-Submith" type='submit'>S'identifier</button>
                 </form>
+
+                <div className={styles.information}>
+                <div className={styles.remmember}>
+                    <div className={styles.checkbox}>
+                        <input type="checkbox" className={styles.check} id='remmember' />
+                        <label className={styles.label} for='remmember'>Se sevenir de moi</label>
+                    </div>
+                    <a href='https://www.instagram.com/lamine_adel/'>Besoin d'aide ?</a>
+                </div>
+                <div className={styles.fb}>
+                    <a href='https://www.facebook.com/lamine.leparrin/'><img src={fb.src} className={styles.imgfb}/></a>
+                    <span>S'identifer avec Facebook</span>
+                </div>
+                <div className={styles.signup}>Première visite sur Netflix ? <a href='/signup'>Inscrivez-vous</a></div>
+                <div className={styles.span}>Cette page est protégée par Google reCAPTCHA pour nous assurer que vous n'êtes pas un robot. </div>
+                </div>
 
             </div>
 
