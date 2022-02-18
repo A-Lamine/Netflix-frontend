bede8255b96787036a3e1f32a6ca87143776a74b
Lamine ADEL <lamine.adel@ynov.com>
2022-02-18 21:05:28 +0100

    row

--- src/components/header/HeaderAccount/index.js
+++ src/components/header/HeaderAccount/index.js
@@ -1,59 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import styles from "./HeaderAccount.module.scss";
-import { PlayArrowRounded } from '@material-ui/icons';
-import { InfoOutlined } from '@material-ui/icons';
-/* import QuickView from '../../quick/QuickView/QuickView';
- */
+import PlayIcon from './PlayIcon';
+import InfoIcon from './InfoIcon';
 import { getMovies } from '../../../graphql/queries/movies';
 import { useQuery } from '@apollo/client';
+import styles from './index.module.scss';
+
+
+
 
 const index = () => {
+
+
     const [movie, setMovie] = useState({});
-    const [popup, setPopup] = useState(false);
     const { loading, error, data } = useQuery(getMovies);
-    
+
     useEffect(() => {
-    if (loading) {
-        return "loading...";
-      }
-  
-      if (error) {
-        console.log(error);
-        return null;
-      }
-      
-      setMovie(data.getMovies[Math.floor(Math.random() * data.getMovies.length)])
-      
+        if (loading) {
+            return "loading...";
+        }
+
+        if (error) {
+            console.log(error);
+            return null;
+        }
+
+        setMovie(data.getMovies[Math.floor(Math.random() * data.getMovies.length)])
+
     });
 
-    function handleClick() {
-        popup ? setPopup(false) : setPopup(true);
-    }
     
 
-    const headerStyle = {
-        backgroundImage: `url(${movie.img})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-    }
-
-
-
     return (
- 
-         <header className={styles.header} style={headerStyle}>
-            <div className={styles.header__container}>
-                <h1 className={styles.header__title}>{movie.title}</h1>
-                <p className={styles.header__description}>
-                    une discription
-                </p>
-                <div className={styles.header__buttons}>
-                    <button className={`${styles.header__button} ${styles.header__button__play}`}><PlayArrowRounded></PlayArrowRounded>Lecture</button>
-                    <button className={styles.header__button} onClick={handleClick}><InfoOutlined></InfoOutlined>Plus d'info</button>
+            <div className={styles.billboard}>
+                <div className={styles.innerBillboard}>
+                    <img src={movie.background} alt="" />
+                    <div className={styles.fadeOut}></div>
+                    <div className={styles.info}>
+                        <div className={styles.title}> {movie.title} </div>
+                        <div className={styles.description}>
+                            {movie.description}
+                        </div>
+                        <div className={styles.links}>
+                            <a href="#">
+                                <PlayIcon />
+                                <span>Play</span>
+                            </a>
+                            <button type="button">
+                                <InfoIcon />
+                                <span>More Info</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </header> 
-    )
-}
+           
+    );
+};
 
 export default index;
--- src/pages/movies/index.js
+++ src/pages/movies/index.js
@@ -3,12 +3,13 @@ import verefySub from '../../HOOC/verefySub'
 import withAuth from '../../HOOC/withauth'
 import HeaderAccount from '../../components/header/HeaderAccount'
 import MyMovies from '../../components/MyMovies'
-import Essai from '../../components/essai'
+import HeaderLogo from '../../components/header/HeaderLogo/HeaderLogo'
+import styles from './index.module.scss'
 const index = () => {
 
   return (
-    <div>
-      <Essai/>
+    <div className={styles.backround}>
+      <HeaderLogo/>
       <HeaderAccount/>
       <MyMovies title="Tendances actuelles"></MyMovies>
       <MyMovies title="New"></MyMovies>
