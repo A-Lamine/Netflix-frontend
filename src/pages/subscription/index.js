f59b9b96dd5e9cc3f762dc1329b74ccdea8622cb
Lamine ADEL <lamine.adel@ynov.com>
2022-02-20 13:26:53 +0100

    subscribtionrow

--- src/pages/subscription/index.js
+++ src/pages/subscription/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import HeaderLogo from "../../components/header/HeaderLogo/HeaderLogo"
 import HeaderToolbar from "../../components/header/HeaderToolbar/HeaderDisc"
 import styles from "./index.module.scss"
-import Separator from "../../components/separator"
+import Checkmarque from "../../../public/checkmarque"
+
+console.log(Checkmarque)
 function index() {
     return (
         <div className={styles.body}>
@@ -10,7 +12,7 @@ function index() {
                 <HeaderLogo />
                 <HeaderToolbar />
             </header>
-            <Separator />
+            
 
             <div className={styles.panelcenter}>
                 <div className={styles.planformcontainer}>
@@ -19,20 +21,60 @@ function index() {
                             Sélectionnez le forfait qui vous convient
                         </div>
                         <div className={styles.liste}>
-                            <div className={styles.info}>Regardez autant que vous voulez. Sans publicité.</div>
-                            <div className={styles.info}>Recommandations personnalisées.</div>
-                            <div className={styles.info}>Changez ou annulez votre forfait à tout moment.</div>
+                            <div className={styles.info_flex}>
+                                <div className={styles.checkmarque}><Checkmarque /></div>
+                                <div className={styles.info}>Regardez autant que vous voulez. Sans publicité.</div>
+                            </div>
+                            <div className={styles.info_flex}>
+                                <div className={styles.checkmarque}><Checkmarque /></div>
+                                <div className={styles.info}>Recommandations personnalisées.</div>
+                            </div>
+                            <div className={styles.info_flex}>
+                                <div className={styles.checkmarque}><Checkmarque /></div>
+                                <div className={styles.info}>Changez ou annulez votre forfait à tout moment.</div>
+                            </div>
                         </div>
-                        
+
                     </div>
                     <div className={styles.tableau}>
-                        
+                        <div className={styles.tableau_row1}>
+                            <input type="button" className={styles.row_btn} value="Standard"></input>
+                            <input type="button" className={styles.row_btn} value="Premium"></input>
 
+                        </div>
+                        <div className={styles.tableau_row2}>
+                            <div>
+                            <span>Abonnement mensuel</span>
+                            </div>
+                            <div className={styles.offres}>
+                            <div className='a'>8,99</div>
+                            <div className='b'>13,49</div>
+                            </div>
+                        </div>
+
+                        <div>
+
+                        </div>
+
+                        <div>
+
+                        </div>
+
+                        <div>
+
+                        </div>
+                    </div>
+
+                    <div className={styles.soustable}>
+                        La disponibilité de la HD (720p), de la Full HD (1080p), de l'Ultra HD (4K) et de la HDR dépend de votre connexion Internet et des capacités de l'appareil. Tous les contenus ne sont pas disponibles dans toutes les résolutions. Pour en savoir plus, veuillez consulter nos Conditions d'utilisation.
+                    </div>
+                    <div className={styles.soustable}>
+                        Seules les personnes qui vivent avec vous peuvent utiliser votre compte. Regardez Netflix en simultané sur 4 appareils différents avec le forfait Premium, sur 2 avec le forfait Standard, et sur 1 avec le forfait Essentiel.
                     </div>
 
                 </div>
                 <div className={styles.btncontainer}>
-                    <button className='btn btn-Submith'>Suivent</button>
+                    <button className={styles.btn_next}>Suivent</button>
                 </div>
 
             </div>
