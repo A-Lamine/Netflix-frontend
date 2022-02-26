04554fa7a5b0683b89f82ab67f4174a216202db9
Lamine ADEL <lamine.adel@ynov.com>
2022-02-26 20:56:49 +0100

    verceltest

--- src/pages/Account/email/index.js
+++ src/pages/Account/email/index.js
@@ -7,16 +7,16 @@ import HeaderConecte from '../../../components/header/HeaderConecte'
 
 
 function index() {
-    const router = useRouter();
-    const [email,setEmail] =  useState(  typeof window !== "undefined" ? (JSON.parse(localStorage.getItem('token')).email) : null);
-    const [token, setToken] = useState(  typeof window !== "undefined" ? (JSON.parse(localStorage.getItem('token'))) : null);
+    const Router = useRouter();
+    const [email,SetEmail] =  useState(  typeof window !== "undefined" ? (JSON.parse(localStorage.getItem('token')).email) : null);
+    const [token, SetToken] = useState(  typeof window !== "undefined" ? (JSON.parse(localStorage.getItem('token'))) : null);
     console.log(token)
    /*  localStorage.setItem('token') */
     const handleSubmit = (e) => {
         e.preventDefault();
         localStorage.setItem("token", JSON.stringify(token))
         alert('email modifier')
-        router.push("/Account")
+        Router.push("/Account")
         
     }
     return (
@@ -26,7 +26,7 @@ function index() {
             <div className={nwstyles.body}>
                 <HeaderConecte />
                 <div className={styles.signinform}>
-                   <div className={nwstyles.pad}> <h1 className={styles.formh1}>Modifier l'adresse mail</h1></div>
+                   <div className={nwstyles.pad}> <h1 className={styles.formh1}>Modifier l&apos;adresse mail</h1></div>
                     <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
                     <Input
                             type="text"
@@ -48,7 +48,7 @@ function index() {
                             placeholder="Nouvelle adresse mail"
                             required={true}
                             onChange={(e) => {
-                                setToken({...token,email:e.target.value})
+                                SetToken({...token,email:e.target.value})
                                 
                             }}
 
