b2d4a5357893efee394219c0d7a4b35eac6d7590
Lamine ADEL <lamine.adel@ynov.com>
2022-02-26 23:55:31 +0100

    vercel-fix

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 export default {
     register(user) {
-        return fetch(`${process.env.API_URL}/api/v1/users/register`, {
+        return fetch(`${process.env.API_URL}api/v1/users/register`, {
             method: "POST",
             headers: {
               "content-type": "application/json",
@@ -9,7 +9,7 @@ export default {
           }).then((res) => res.json())
     },
     login(user) {
-        return fetch(`${process.env.API_URL}/api/v1/users/login`, {
+        return fetch(`${process.env.API_URL}api/v1/users/login`, {
             method: "POST",
             headers: {
               "content-type": "application/json",
@@ -19,7 +19,7 @@ export default {
     },
     
     verifyToken(token) {
-        return fetch(`${process.env.API_URL}/api/v1/users/verifytoken`, {
+        return fetch(`${process.env.API_URL}api/v1/users/verifytoken`, {
             headers: {
                 "authorization":token
             }
