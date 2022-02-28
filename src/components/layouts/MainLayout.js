de1c76a1c504ba3a50cfa2e413574095476f96b6
Lamine ADEL <lamine.adel@ynov.com>
2022-02-28 22:01:22 +0100

    url-icon

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from "./MainLayout.module.scss";
 import Footer from '../Footer/Footer';
-
+import Head from 'next/head'
 
 const Mainlayout = ({ children }) => {
     return (
         <>
+            <Head>
+                <title>Netflix France</title>
+                <link rel="icon" href="/icon.ico" type="image/icon type" />
+            </Head>
             <main>
                 <div>
                     {children}
